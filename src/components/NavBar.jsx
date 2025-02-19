84d03dfc98c37db7aaff9407cf8b93f82731dc6d
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-02-19 11:12:22 +0200

    toggle sidebar for mobile screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import SideBar from "./SideBar";
 
 const NavBar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <header className="border-b  bg-white ">
-      <nav className="flex items-center justify-between md:justify-end py-5 max-container">
+    <header className="bg-white border-b ">
+      <nav className="flex items-center justify-between py-5 md:justify-end max-container">
         <div className="flex items-center space-x-3 md:space-x-0 md:hidden">
-          <div>
-            <GiHamburgerMenu />
-          </div>
+          <GiHamburgerMenu className="cursor-pointer" onClick={openSidebar} />
           <Link
             to="/"
             className="font-extrabold text-2xl tracking-[-0.8px]  text-gray-900"
@@ -19,6 +29,17 @@ const NavBar = () => {
         </div>
         <FaUserCircle size={30} />
       </nav>
+      {isSidebarOpen && (
+        <div className="fixed inset-0 flex md:hidden">
+          <div
+            onClick={closeSidebar}
+            className="fixed inset-0 bg-black opacity-50"
+          ></div>
+          <div className="min-h-screen  w-[70%] sm:w-[50%] relative z-10 top-0 ">
+            <SideBar closeSidebar={closeSidebar} />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,27 @@
 import { Link, NavLink } from "react-router-dom";
 import { navLinks } from "../constance";
+import { IoAddSharp } from "react-icons/io5";
 
-const SideBar = () => {
+const SideBar = ({ closeSidebar }) => {
   return (
-    <div className="pt-5 px-5 ">
-      <Link
-        to="/"
-        className="font-extrabold text-gray-900 text-2xl tracking-[-0.8px] pl-2"
-      >
-        audiophile
-      </Link>
-      <ul className="font-medium text-gray-900 text-base  mt-5 capitalize space-y-1">
+    <div className="min-h-screen px-5 pt-5 bg-white">
+      <div className="flex items-center justify-between">
+        <Link
+          to="/"
+          className="font-extrabold text-gray-900 text-2xl tracking-[-0.8px] pl-2"
+        >
+          audiophile
+        </Link>
+        <div
+          onClick={closeSidebar}
+          className="p-1 border rounded-full cursor-pointer md:hidden"
+        >
+          <IoAddSharp size={24} className="rotate-45" />
+        </div>
+      </div>
+      <ul className="mt-5 space-y-1 text-base font-medium text-gray-900 capitalize">
         {navLinks.map((item) => (
-          <li>
+          <li key={item.name}>
             <NavLink
               to={item.path}
               className={({ isActive }) =>
